5ada23de0761da566d32c695ff6d7e069401dac1
emaposse <eusebiomaposse@gmail.com>
2021-08-04 22:36:39 +0200

    add inoveted IT  solution.

---
+++ omod/src/main/java/org/openmrs/module/smsreminder/scheduler/SendSMS.java
@@ -0,0 +1,19 @@
+package org.openmrs.module.smsreminder.scheduler;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+public class SendSMS {
+
+	public  static String sms(String to, String text) throws UnirestException {
+		HttpResponse<String> response; response = Unirest.post("https://api.infobip.com/sms/1/text/single")
+
+				.header("authorization", "Basic QXJ0c29mdFRlYzpNYXRzMTAxMCE=")
+				.header("content-type", "application/json").header("accept", "application/json")
+				.body("{\"from\":\"FGH\",\"to\":\"+" + to + "\",\"text\":\"".concat(text) + "\"}").asString();
+
+		return response.getBody();
+}
+
+}
