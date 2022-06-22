a2baec8972dbf5e912068d3e23a64f4720baef09
emaposse <eusebiomaposse@gmail.com>
2022-06-22 16:44:22 +0200

    TRD.

--- omod/src/main/java/org/openmrs/module/smsreminder/scheduler/SendSmsReminderTask.java
+++ omod/src/main/java/org/openmrs/module/smsreminder/scheduler/SendSmsReminderTask.java
@@ -1,21 +1,8 @@
 package org.openmrs.module.smsreminder.scheduler;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.openmrs.api.context.Context;
-import org.openmrs.scheduler.tasks.AbstractTask;
-
 /**
  * Created by nelson.mahumane on 20-10-2015.
  */
-public class SendSmsReminderTask extends AbstractTask {
-	private final Log log = LogFactory.getLog(this.getClass());
+public class SendSmsReminderTask  {
 
-	@Override
-	public void execute() {
-
-		Context.openSession();
-		this.log.info("Starting send SMS ... ");
-		this.log.info("End send SMS ... ");
-}
 }
