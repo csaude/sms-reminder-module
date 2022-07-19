b4e38607aa1e411bf4b58543ca4569767bee4655
emaposse <eusebiomaposse@gmail.com>
2022-07-19 17:15:52 +0200

    sms rimender module.

--- omod/src/main/java/org/openmrs/module/smsreminder/web/controller/SMSController.java
+++ omod/src/main/java/org/openmrs/module/smsreminder/web/controller/SMSController.java
@@ -6,11 +6,9 @@ import org.openmrs.module.smsreminder.utils.SmsReminderResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@SessionAttributes("notificationPatients")
 public class SMSController {
 
 
