9c9304e067841accda9ce0ac9ae6ef08fb7bc2c1
emaposse <eusebiomaposse@gmail.com>
2022-08-02 16:12:17 +0200

    update sent table.

--- api/src/main/java/org/openmrs/module/smsreminder/api/db/hibernate/HibernateSmsReminderDAO.java
+++ api/src/main/java/org/openmrs/module/smsreminder/api/db/hibernate/HibernateSmsReminderDAO.java
@@ -100,7 +100,6 @@ public class HibernateSmsReminderDAO implements SmsReminderDAO {
 		return c.list();
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 
 	public Sent getSentByMsgId(final Integer id) throws DAOException {
